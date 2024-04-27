3ca67fe4e9e6e920069d75ada2da1e29439cd377
ziad-bakki <bakkizia@msu.edu>
2024-04-27 02:34:30 -0400

    implemented a small template frontend

--- Parse.py
+++ Parse.py
@@ -1,19 +1,35 @@
+from flask import Flask, jsonify, render_template, request
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.nasa.gov/image-of-the-day/page/'
+app = Flask(__name__)
 
-first_page = 1
-last_page = 100
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-image_urls = []
-for page_number in range(first_page, last_page + 1):
-    new_url = URL + str(page_number) + '/'
-    request = requests.get(new_url)
-    if request.status_code == 200:
-        soup = BeautifulSoup(request.content, 'html5lib')
-        images = soup.find_all("div", class_="hds-gallery-item-single hds-gallery-image")
-        image_urls = [img.find('img')['src'] for img in images if img.find('img')]
-        print(image_urls)
+@app.route('/get_images/')
+def get_images():
+    keyword = request.args.get('keyword', '')  # Use Flask's request to get query parameters
+    URL = 'https://www.nasa.gov/image-of-the-day/page/'
+    first_page = 1
+    last_page = 10
+    image_data = {}
+    count = 0
+    for page_number in range(first_page, last_page + 1):
+        new_url = URL + str(page_number) + '/'
+        response = requests.get(new_url)  # Rename variable to avoid confusion with Flask's request
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html5lib')
+            images = soup.find_all("div", class_="hds-gallery-item-single hds-gallery-image")
+            image_urls = {index: img.find('img')['src'] for index, img in enumerate(images, start=1) if img.find('img')}
+            for src in image_urls.values():
+                
+                if keyword.lower() in src.lower():  # Ensure case insensitive comparison
+                    image_data[count] = src
+                    count += 1
             
+    return jsonify(image_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
